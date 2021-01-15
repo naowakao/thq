e15dcbdcaf5215975e1da37cf1ab2fcddbabb2a9
nao chimura <65644019+naowakao@users.noreply.github.com>
2021-01-15 14:36:51 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,168 @@
+<?php
+  require_once('../../wp-load.php');
+  global $g_page_title;
+  $g_page_title = "ご来場予約特典 [こだわりの逸品とコラボ]";
+  get_header();
+?>
+
+<link rel="stylesheet" href="css/layout_vol.03.css">
+
+
+<main id="mainContainer" class="campaignMainContainer">
+  
+  
+  <!--eye-catch-->
+  <section id="eye-catch">
+    <h1>
+      <img src="img_vol.03/main_pc.jpg" alt="ご来場予約特典[開催期間：3/7（土）〜5/31（日）] 第1弾こだわりの銘店とコラボ こだわりの逸品 トヨタホーム九州×糸島磯本農園" class="pc"/>
+      <img src="img_vol.03/main_sp.jpg" alt="ご来場予約特典[開催期間：3/7（土）〜5/31（日）] 第1弾こだわりの銘店とコラボ こだわりの逸品 トヨタホーム九州×糸島磯本農園" class="sp"/>
+    </h1>
+  </section>
+  <!--//eye-catch end-->
+
+  
+  <article id="content">
+    
+    <!--s01-->
+    <section id="s01">
+			<div class="in-box">
+        <h2>ご来場&Instagramをフォローで<br><span>糸島磯本農園「あまおう飲む酢＆コンポート」</span>をプレゼント！</h2>
+        <!--inst-box-->
+        <div class="inst-box">        
+          <span class="inst-btn-box"><a href="https://www.instagram.com/toyotahome_kyushu/" target="_blank"><img src="img_vol.03/inst_btn.png" alt="トヨタホーム九州 公式Instgram 各月先着20名様 3/7（土）〜5/31（日）"/></a></span>
+          <p class="text-box">
+            トヨタホーム九州 公式Instagramをフォローして、<br>以下会場いずれかをご予約後、<br class="sp">ご来場いただいた方が対象となります。
+            <span>開催期間：3/7（土）〜5/31（日）</span>
+          </p>
+          <ul>
+            <li><a href="<?php echo site_url('','https'); ?>/showroom/"><img src="img_vol.03/btn_01.png" alt="展示場来場予約"/></a></li>
+            <li><a href="<?php echo site_url('','https'); ?>/machikado/"><img src="img_vol.03/btn_02.png" alt="街かど来場予約"/></a></li>
+            <li><a href="<?php echo site_url('','https'); ?>/realestate?type=tochi"><img src="img_vol.03/btn_03.png" alt="土地見学予約"/></a></li>
+            <li><a href="<?php echo site_url('','https'); ?>/realestate?type=tateuri"><img src="img_vol.03/btn_04.png" alt="建売見学予約"/></a></li>
+          </ul>
+        </div>
+        <!--inst-box-->
+      </div>      
+		</section>
+    <!--//s01 end-->
+    
+    <!--s02-->
+    <section id="s02">
+			<div class="in-box">
+        <span class="label-box"><img src="img_vol.03/s02_i01.png" alt="こだわりの銘店とコラボ 第1弾はコチラ！"/></span>
+        <h2><span>こだわりの逸品ご紹介</span>糸島磯本農園<br class="sp">「あまおう飲む酢＆コンポート」</h2>
+        <ul>
+          <li>
+            <p>
+		    海と山、豊かな自然に囲まれた糸島。
+            <br>
+            そんな糸島半島の志摩吉田地区で季節ごとに変わりゆく草木や虫たちを眺めながら,
+            いちごを生産している磯本農園。有機肥料や微生物資材を用いていちごが育ちやすい環境を整え、いちご自身の力で育っていくための手助けをすることを心がけることが磯本農園のこだわりです。
+            <br><br>
+            いちごを知り尽くした磯本農園がひと手間加え、いちごのおいしさを更に引き出した「あまおう飲む酢＆コンポート」。こちらの商品を『ご来場＆トヨタホーム九州　公式Instagramをフォロー』していただいた方（各月先着20名様）にプレゼントいたします。
+            <span class="img-01box"><img src="img_vol.03/s02_i02.png" alt="農園IMG"/></span>
+            </p>
+          </li>
+          <li>
+            <span class="img-02box"><img src="img_vol.03/s02_i03.jpg" alt="ITOSHIMA ISOMOTO FARM"/></span>
+          
+		    <span class="img-03box"><img src="img_vol.03/s02_i04.png" alt="「贅沢にも飲む酢とコンポートの両方をヨーグルトにかけて食べました。フレッシュのあまおう食べる以上に、あまおうを強く感じる！！こんなに濃厚ないちごを味わえるとは驚きました！！美味しかったです！トヨタホーム九州 ホームアドバイザーの感想"/></span>
+			  
+		  </li>
+        </ul>
+      </div>      
+		</section>
+    <!--//s02 end-->
+    
+    <!--s03-->
+    <section id="s03">
+			<div class="in-box">
+        <div class="wrapper">
+          <div class="text-box">
+            <h3>今回コラボいただいた<br>銘店はこちら</h3>
+            <h4>磯本農園直営カフェ</h4>
+			<h4>『cafe TANNAL（カフェ タンナル）』</h4>
+			<div class="detail-box">
+              <p>〒8191301 糸島市志摩井田原 85-4</p>
+              <p>営業時間：10:30〜18:00</p>
+              <p>定休日：無し（10月11月は毎週水曜日）</p>
+              <p>駐車場：あり６台</p>
+              <p>TEL：<a href="tel:0923327643" class="tel">092-332-7643</a></p>
+			</div>
+            <span class="btn-box"><a href="https://www.instagram.com/cafe_tannal/" target="_blank"><img src="img_vol.03/s03_btn.png" alt="cafe_tannal"/></a></span>
+          </div>
+          <div class="img-box"><img src="img_vol.03/s03_i01.jpg" alt="磯本農園直営カフェ cafe TANNAL（カフェ　タンナル）"/></div>
+        </div>
+      </div>      
+		</section>
+    <!--//s03 end-->
+    
+    <!--s04-->
+    <section id="s04">
+			<div class="in-box">
+        <h2>プレゼントお申し込み方法</h2>
+        <span class="flow-box">
+          <img src="img_vol.03/s04_flow_pc.png" alt="" class="pc"/>
+          <img src="img_vol.03/s04_flow_sp.png" alt="" class="sp"/>
+        </span>
+        <!--inst-box-->
+        <div class="inst-box">        
+          <span class="inst-btn-box"><a href="https://www.instagram.com/toyotahome_kyushu/" target="_blank"><img src="img_vol.03/inst_btn.png" alt="トヨタホーム九州 公式Instgram 各月先着20名様 3/7（土）〜5/31（日）"/></a></span>
+          <p class="text-box">
+            トヨタホーム九州 公式Instagramをフォローして、<br>以下会場いずれかをご予約後、<br class="sp">ご来場いただいた方が対象となります。
+            <span>開催期間：3/7（土）〜5/31（日）</span>
+          </p>
+          <ul>
+            <li><a href="<?php echo site_url('','https'); ?>/showroom/"><img src="img_vol.03/btn_01.png" alt="展示場来場予約"/></a></li>
+            <li><a href="<?php echo site_url('','https'); ?>/machikado/"><img src="img_vol.03/btn_02.png" alt="街かど来場予約"/></a></li>
+            <li><a href="<?php echo site_url('','https'); ?>/realestate?type=tochi"><img src="img_vol.03/btn_03.png" alt="土地見学予約"/></a></li>
+            <li><a href="<?php echo site_url('','https'); ?>/realestate?type=tateuri"><img src="img_vol.02/btn_04.png" alt="建売見学予約"/></a></li>
+          </ul>
+        </div>
+        <!--inst-box-->
+      </div>      
+		</section>
+    <!--//s04 end-->
+    
+    <!--s05-->
+    <section id="s05">
+			<div class="in-box">
+        <h2>来場予約のメリット</h2>
+        <ul>
+          <li><img src="img_vol.03/s05_i01.png" alt="待ち時間なく、優先してご対応"/></li>
+          <li><img src="img_vol.03/s05_i02.png" alt="知りたい内容を明確にアドバイス"/></li>
+          <li><img src="img_vol.03/s05_i03.png" alt="ご希望にあわせてご案内"/></li>
+        </ul>
+        <span class="img-box"><img src="img_vol.03/s05_i04.jpg" alt=""/></span>
+        <p>
+          <strong>ご来場予約特典 コラボ企画について</strong>
+          ■ご応募条件/2020年3/7（土）〜5/31（日）までにご来場予約のお申し込みいただき、トヨタホーム九州の展示場・まち角モデル会場・土地見学・建売見学にお越しいただき、トヨタホーム九州公式Instagramをフォローして、その画面をご提示いただいた方。先着数に達し次第、終了とさせていただきます。おひと家族につき、1名様がお申込みいただけます。 ※写真は全てイメージです。
+        </p>
+        <span class="bnr-box"><img src="img_vol.03/s05_bnr.jpg" alt="2020 Summer Coming Soon 次のコラボは？ご期待ください！！ 第2弾[こだわりの銘店とコラボ]こだわりの逸品 トヨタホーム九州×??????"/></span>
+      </div>      
+		</section>
+    <!--//s05 end-->
+    
+    
+	</article><!--#content-->
+</main><!--#container-->
+    
+    
+    
+<!--resource-->
+<!--css読み込み-->
+<script type="text/javascript">
+var link = document.createElement('link');
+link.href = 'css/layout_vol.03.css';
+link.rel = 'stylesheet';
+link.type = 'text/css';
+var head = document.getElementsByTagName('head')[0];
+head.appendChild(link);
+</script>
+<!--//css読み込み end-->
+<!--//resource end-->
+
+
+<?php
+  get_footer();
+?>
